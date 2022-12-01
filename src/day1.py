a6889e3a1e65ad807a38752bf6c6a5618718dd82
David Simon <D@vidSimon.tech>
2022-12-01 05:53:10 +0000

    Factor out input parsing, solve d1p2

--- src/day1.py
+++ src/day1.py
@@ -1,21 +1,36 @@
-import click
+import heapq
 import sys
+from typing import Generator, TextIO
+
+import click
+
 from common import common_options
-from typing import TextIO
 
 
-def do_part1(inp: TextIO) -> int:
-    max_elf_total = 0
+def gen_elf_totals(inp: TextIO) -> Generator[int, None, None]:
     this_elf_total = 0
     for line in inp:
         line = line.rstrip()
         if line:
             this_elf_total += int(line)
         else:
-            max_elf_total = max(max_elf_total, this_elf_total)
+            yield this_elf_total
             this_elf_total = 0
 
-    return max_elf_total
+
+def do_part1(inp: TextIO) -> int:
+    return max(gen_elf_totals(inp))
+
+
+def do_part2(inp: TextIO) -> int:
+    heap = []
+    for elf_total in gen_elf_totals(inp):
+        if len(heap) < 3:
+            heapq.heappush(heap, elf_total)
+        else:
+            heapq.heappushpop(heap, elf_total)
+
+    return sum(heap)
 
 
 @click.command()
@@ -23,3 +38,5 @@ def do_part1(inp: TextIO) -> int:
 def day1(part: int):
     if part == 1:
         print(do_part1(sys.stdin))
+    else:
+        print(do_part2(sys.stdin))
