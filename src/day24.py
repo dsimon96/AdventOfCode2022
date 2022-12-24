cf0dd04d1e2f4172f458f7ef3f25169eb7170807
David Simon <D@vidSimon.tech>
2022-12-24 21:59:49 +0000

    WIP part1

---
+++ src/day24.py
@@ -0,0 +1,9 @@
+from typing import TextIO
+
+Blizzards = dict[int,]
+
+def part1(inp: TextIO) -> int:
+    s = [line.rstrip() for line in inp.readlines()]
+    left_blizzards =
+    startx = s[0].index(".")
+    endx = s[-1].index(".")
